df3cbcba413a95ce9390dd3e491851dd3ef93b25
Joseph Opio <josephopio11@gmail.com>
2022-11-30 20:45:33 +0300

    Ignoring livewire

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,10 @@ require __DIR__.'/auth.php';
 Route::middleware('auth')->group(function () {
     Route::view('about', 'about')->name('about');
 
-    Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    Route::prefix('hr')->group(function () {
+        Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
+    });
+
 
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
